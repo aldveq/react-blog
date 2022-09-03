c4e6d12d5da97ff5a0c42ae3e5c9904769476581
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-09-03 10:25:04 -0600

    added single post data endpoint

--- src/pages/ArticleSinglePage.js
+++ src/pages/ArticleSinglePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MainWrapper from "../containers/MainWrapper";
 import PostsWrapper from "../containers/PostsWrapper";
@@ -11,11 +11,29 @@ const ArticleSinglePage = () => {
 	const blogSingleData = blogData.find(bData => bData.slug === name);
 	const moreArticles = blogData.filter(bData => bData.slug !== name);
 
+	const [postData, setPostData] = useState({ upvotes: 0, comments:[] });
+	const [loader, setLoader] = useState(false);
+
+	useEffect(() => {
+		const fetchPostData = async () => {
+			setLoader(true);
+			const postDataFromServer = await fetch(`/api/posts/${name}`);
+			const jsonPostDataFromServer = await postDataFromServer.json();
+			const { upvotes, comments } = jsonPostDataFromServer;
+			setPostData({ upvotes, comments });
+			setLoader(false);
+		};
+		fetchPostData();
+	}, [name]);
+
+	if (loader) return <MainWrapper type='body'> <h3>Loading...</h3> </MainWrapper>
+
 	if ( blogSingleData === undefined || blogSingleData === null ) return <NotFoundPage/>
 
 	return (
 		<MainWrapper type='body'>
 			<Title text={blogSingleData.title} type='main' />
+			<p>This post has been upvoted {postData.upvotes} times!</p>
 			<p>{blogSingleData.content}</p>
 			<br />
 			<Title text='More Articles' type='secondary' />
