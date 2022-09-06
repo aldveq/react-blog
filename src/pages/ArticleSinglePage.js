fdae46892c0ac69dec2bbe70e2fd1788c793005c
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-09-05 22:06:18 -0600

    added title & content attributes for posts

--- src/pages/ArticleSinglePage.js
+++ src/pages/ArticleSinglePage.js
@@ -11,7 +11,7 @@ const ArticleSinglePage = () => {
 	const blogSingleData = blogData.find(bData => bData.slug === name);
 	const moreArticles = blogData.filter(bData => bData.slug !== name);
 
-	const [postData, setPostData] = useState({ upvotes: 0, comments:[] });
+	const [postData, setPostData] = useState({ upvotes: 0, comments: [], title: '', content: '' });
 	const [loader, setLoader] = useState(false);
 
 	useEffect(() => {
@@ -19,8 +19,8 @@ const ArticleSinglePage = () => {
 			setLoader(true);
 			const postDataFromServer = await fetch(`/api/posts/${name}`);
 			const jsonPostDataFromServer = await postDataFromServer.json();
-			const { upvotes, comments } = jsonPostDataFromServer;
-			setPostData({ upvotes, comments });
+			const { upvotes, comments, title, content } = jsonPostDataFromServer;
+			setPostData({ upvotes, comments, title, content });
 			setLoader(false);
 		};
 		fetchPostData();
@@ -30,20 +30,19 @@ const ArticleSinglePage = () => {
 		<>
 			<br />
 			<Title text='Comments' type='secondary' />
-			<CommentsList comments={postData.comments} postName={name} setPostData={setPostData} />
+			<CommentsList comments={postData.comments} postName={name} />
 		</>
 	);
 	
-
 	if (loader) return <MainWrapper type='body'> <h3>Loading...</h3> </MainWrapper>
 
 	if ( blogSingleData === undefined || blogSingleData === null ) return <NotFoundPage/>
 
 	return (
 		<MainWrapper type='body'>
-			<Title text={blogSingleData.title} type='main' />
-			{ parseInt(postData.upvotes) !== 0 ? <UpvoteCounter upvotes={postData.upvotes} postName={name} setPostData={setPostData} /> : null }
-			<p>{blogSingleData?.content}</p>
+			{ postData?.title !== '' ? <Title text={postData?.title} type='main' /> : null }
+			{ parseInt(postData?.upvotes) !== 0 ? <UpvoteCounter upvotes={postData?.upvotes} postName={name} setPostData={setPostData} /> : null }
+			{ postData?.content !== '' ? <p>{postData?.content}</p> : null }
 			{ postData?.comments.length > 0 ? drawComments() : null }
 			<AddCommentForm postName={name} setPostData={setPostData}/>
 			<br />
