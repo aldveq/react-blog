3fa1bab40760c7f8eb8f0ffb7802f428b027088f
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-10-17 19:31:08 -0600

    added user token to add comment form component

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,46 +1,37 @@
 import React from "react";
-import { useInput } from "../hooks";
+import { useInput, useUser } from "../hooks";
 
 const AddCommentForm = ({postName, setPostData}) => {
-	const [user, setUser] = useInput('');
 	const [comment, setComment] = useInput('');
+	const { user } = useUser();
 
 	const addComment = async (e) => {
 		e.preventDefault();
 
-		if (user?.value === '' || comment?.value === '') {
+		if ( comment?.value === '' ) {
 			alert('Please, fill out the inputs!');
 			return;
 		}
 
+		const token = user && await user.getIdToken();
+		const headers = token ? { 'Content-Type': 'application/json', authtoken: token } : { 'Content-Type': 'application/json' };
+
 		const result = await fetch(`/api/posts/${postName}/add-comment`, {
 			method: 'post', 
-			body: JSON.stringify({user: user?.value, comment: comment?.value}), 
-			headers: {
-				'Content-Type': 'application/json'
-			}
+			body: JSON.stringify({user: user?.email, comment: comment?.value}), 
+			headers,
 		});
 		const resultJson = await result.json();
 		setPostData(resultJson);
-		setUser('');
 		setComment('');
 	}
 
 	return (
 		<form className="add-comment-form" onSubmit={addComment}>
 			<h3>Add a new comment!</h3>
+			{ user && <div className="add-comment-form__form-group"><p>You are posting as {user?.email}</p></div> }
 			<div className="add-comment-form__form-group">
-				<label htmlFor="user">User</label>
-				<input 
-					type="text" 
-					id="user" 
-					{...user}
-				/>
-			</div>
-			<div className="add-comment-form__form-group">
-				<label htmlFor="comment">Comment</label>
 				<textarea 
-					id="comment" 
 					cols="30" 
 					rows="10" 
 					{...comment}
