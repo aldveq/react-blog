b113538146ee8142544ce83051f3182d2c445116
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-10-28 17:31:04 -0600

    added open/close button to open/close mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate, Link } from "react-router-dom";
 import MainWrapper from "../containers/MainWrapper";
 import { getAuth, signOut } from "firebase/auth";
@@ -7,6 +7,8 @@ import { useUser } from '../hooks';
 const Header = () => {
 	const { user } = useUser();
 	const navigate = useNavigate();
+
+	const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
 	const drawLogoutButton = () => (
 		<li>
@@ -32,7 +34,16 @@ const Header = () => {
 		<nav className="nav-wrapper">
 			<MainWrapper type='header'>
 				<div className="nav-wrapper__logo-text"><Link to='/'><span>React</span>Blog</Link></div>
-				<div className="nav-wrapper__navigation-container">
+				<button 
+					className="nav-wrapper__open-menu-btn"
+					onClick={() => setIsMobileNavOpen(true)}
+				>Menu</button>
+				<div className={`nav-wrapper__navigation-container ${ isMobileNavOpen && 'active' }`}>
+					<button 
+						title="Close menu"
+						className="close-menu-btn"
+						onClick={() => setIsMobileNavOpen(false)}
+					>Close</button>
 					{ user && <div className="nav-wrapper__user-container"><p>Welcome: <span>{user.email}</span></p></div> }
 					<ul>
 						<li>
